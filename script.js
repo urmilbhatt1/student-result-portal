80394a19735a64edf9ed2192147692f2096e1315
urmilbhatt1 <140245197+urmilbhatt1@users.noreply.github.com>
2023-10-07 12:37:24 +0530

    script.js

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ function checkResult() {
 
     // Sample student data (for demonstration purposes)
     var studentData = {
-        "13": { "marks": 9.5/20},
-        "06": { "marks": 14/20},
-        "07": { "marks": 13.5/20},
-        "03": { "marks": 12/20},
-        "05": { "marks": 11/20},
-        "01": { "marks": 10/20},
-        "11": { "marks": 10/20},
-        "14": { "marks": 11/20},
-        "16": { "marks": 7.5/20},
-        "02": { "marks": 14/20},
-        "10": { "marks": 13/20}
+        "13": { "marks": 9.5 Out of 20},
+        "06": { "marks": 14 Out of 20},
+        "07": { "marks": 13.5 Out of 20},
+        "03": { "marks": 12 Out of 20},
+        "05": { "marks": 11 Out of 20},
+        "01": { "marks": 10 Out of 20},
+        "11": { "marks": 10 Out of 20},
+        "14": { "marks": 11 Out of 20},
+        "16": { "marks": 7.5 Out of 20},
+        "02": { "marks": 14 Out of 20},
+        "10": { "marks": 13 Out of 20}
     };
 
     // Check if the roll number exists in the student data
     if (studentData.hasOwnProperty(rollNumber)) {
         var result = studentData[rollNumber];
-        resultContainer.innerText = "Subject: " + result.subject + ", Marks: " + result.marks;
+        resultContainer.innerText = Marks: " + result.marks;
         resultContainer.classList.remove("error");
         resultContainer.classList.add("success");
     } else {
